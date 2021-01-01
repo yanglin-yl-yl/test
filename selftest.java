7442a70830c756db5599748bdd84911c7ff6afa6
yanglin-yl-yl <76813047+yanglin-yl-yl@users.noreply.github.com>
2021-01-01 08:34:07 +0800

    第一次修改编辑创建的selftest文件
    这次编辑进行了增加了一个普通的减法操作

--- selftest.java
+++ selftest.java
@@ -2,6 +2,11 @@ public static void main (String [] args){
    int a = 3;
    int b = 4;
    int c = a + b;
-   System.out.println(c)
+   System.out.println(c);
    
+      
+      int d = 5 ;
+      int e = 6 ;
+      int f = e - d;
+   System.out.println(f);
 }
